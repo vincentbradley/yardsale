9729e5bcf2c75dcc1c9043b45b42473de8582376
vincentbradley <bradley.vince@gmail.com>
2014-12-07 19:35:31 -0700

    Added a pop-up tab for submitting new sales

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -76,3 +76,38 @@ angular.module('starter.controllers', [])
         return element.name.match() ? true : false;
 
     })
+
+    .controller('submitCtrl',function($scope, $ionicPopup) {
+
+        //Pop Up for new sale
+        $scope.showSubmit = function() {
+            $scope.data = {}
+
+
+            var salePopup = $ionicPopup.show({
+                template: '<input type="text" ng-model="data.saleInfo" ng-maxlength="25">',
+                title: 'Please Enter Your Yardsale Title',
+                subTitle: 'Please use Less Than 25 Characters',
+                scope: $scope,
+                buttons: [
+                    { text: 'Cancel' },
+                    {
+                        text: '<b>Save</b>',
+                        type: 'button-positive',
+                        onTap: function(e) {
+                            if (!$scope.data.saleInfo) {
+                                e.preventDefault();
+                            } else {
+                                return $scope.data.saleInfo;
+                            }
+                        }
+                    }
+                ]
+            });
+            salePopup.then(function(res) {
+                console.log('Tapped!', res);
+            });
+
+        };
+
+    });
